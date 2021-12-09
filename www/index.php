114ccfaffc9f3da4f6caa367480e663d98cb5b90
antoskins <antonypinel@gmail.com>
2021-12-09 15:49:52 +0000

    added examples for activities with different placeholders

--- www/index.php
+++ www/index.php
@@ -3,9 +3,60 @@ include 'utils.inc.php';
 start_page();
 ?>
 
-<h1>My First Heading</h1>
-		<p>Niggers be like.</p>
-
+<h1>Voici les idées actuelles.</h1>
+		<p>Exemples uwu :</p>
+       
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
+</div>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
+</div>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
+</div>
 <?php
     end_page();
 ?>
