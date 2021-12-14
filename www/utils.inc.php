63eb9b3fd6e85ab57277773a1760a2dc36c647e6
antoskins <antonypinel@gmail.com>
2021-12-14 07:54:38 +0000

    update functions

--- www/utils.inc.php
+++ www/utils.inc.php
@@ -7,9 +7,14 @@ function start_page()
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	</head>
-	<body>
+	<body>';
+}
+?>
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<?php
+function nav_bar()
+{
+  echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Fuck kylian</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,7 +46,9 @@ function start_page()
   </div>
 </nav>';
 }
+?>
 
+<?php
 function end_page()
 {
     echo '<footer class="bg-light text-center">
