aba693312bb89ca5edd2660eadad7ca1d3cc1a58
ozeliurs <ozeliurs@gmail.com>
2021-12-14 11:00:53 +0100

    Test Controller

--- www/index.php
+++ www/index.php
@@ -1,27 +1,26 @@
 <?php
 include 'Controller/index_controller.php';
 
+$ctrl = new Controller();
 
+if (!isset($_GET["action"])) {
+    echo $ctrl->create();
+} else {
+    switch ($_GET["action"]) {
+        case "create":
+            echo $ctrl->create();
+            break;
 
-$ctrl = Controller();
+        case "read":
+            echo $ctrl->read();
+            break;
 
-switch ($_GET["action"]) {
-    case "create":
-        echo $ctrl->create();
-        break;
+        case "update":
+            echo $ctrl->update();
+            break;
 
-    case "read":
-        echo $ctrl->read();
-        break;
-
-    case "update":
-        echo $ctrl->update();
-        break;
-
-    case "delete":
-        echo $ctrl->delete();
-        break;
-
-    default:
-        echo $ctrl->create();
+        case "delete":
+            echo $ctrl->delete();
+            break;
+    }
 }
