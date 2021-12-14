1d8000402787aef20d2523b3808803a56c3d6822
antoskins <antonypinel@gmail.com>
2021-12-14 07:58:46 +0000

    hgh

--- www/welcome.php
+++ www/welcome.php
@@ -4,29 +4,6 @@ start_page();
 nav_bar();
 ?>
 
-<h1>cc à tous les bg</h1>
-  <h3>pas encore connecté ? allez viens on est bien :<h3>
-
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-   <label for="inputPassword5" class="form-label">Password</label>
-   <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
-   <div id="passwordHelpBlock" class="form-text">
-    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-</div>
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
 <h3>oh faut créer un compte ta mère là :<h3>
 
 <div class="input-group mb-3">
