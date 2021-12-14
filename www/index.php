fd84ce1efacb083d405b906dd908374ba70e54a0
RedCharriot <martin.MOREIRA-DE-JESUS@etu.univ-amu.fr>
2021-12-14 09:26:42 +0100

    added files

--- www/index.php
+++ www/index.php
@@ -1,65 +1,26 @@
 <?php
-include 'utils.inc.php';
+require_once 'utils.inc.php';
+require_once 'Controllers/IndexController.php';
+
+$controller = new IndexController();
+
 start_page();
-?>
 
+$action = $_GET['action'];
+switch ($action) {
+    case 'read':
+        echo $controller->read();
+        break;
+    case 'create':
+        echo $controller->create();
+        break;
+    case 'update':
+        echo $controller->update();
+        break;
+    case 'delete':
+        echo $controller->delete();
+        break;
+}
 
-<h1>Voici les idées actuelles.</h1>
-		<p>Exemples uwu et awa:</p>
-       
-<div class="card" name="card1">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
-</div>
-<div class="card" name="card2">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
-</div>
-<div class="card" name="card3">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
-</div>
+end_page();
 
-
-<?php
-    end_page();
-?>
--- www/utils.inc.php
+++ www/utils.inc.php
@@ -103,4 +103,7 @@ function end_page()
       </body>
   </html>';
 }
+
 ?>
+
+
