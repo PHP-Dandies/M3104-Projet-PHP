2626e13f56dc5d47ded729fa0bde2af2db321aea
ozeliurs <ozeliurs@gmail.com>
2021-12-14 09:33:29 +0100

    Test Controller

--- www/index.php
+++ www/index.php
@@ -1,65 +1,27 @@
 <?php
-include 'utils.inc.php';
-start_page();
-?>
+include 'Controller/index_controller.php';
 
 
-<h1>Voici les idées actuelles.</h1>
-		<p>Exemples uwu et awa:</p>
-       
-<div class="card" name="card1">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
-</div>
-<div class="card" name="card2">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
-</div>
-<div class="card" name="card3">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  <img src="http://ekladata.com/pEGj8d3WVbJc-8gEahiQ03t2TLY.png" class="card-img-bottom" alt="...">
-</div>
 
+$ctrl = Controller();
 
-<?php
-    end_page();
-?>
+switch ($_GET["action"]) {
+    case "create":
+        echo $ctrl->create();
+        break;
+
+    case "read":
+        echo $ctrl->read();
+        break;
+
+    case "update":
+        echo $ctrl->update();
+        break;
+
+    case "delete":
+        echo $ctrl->delete();
+        break;
+
+    default:
+        echo $ctrl->create();
+}
