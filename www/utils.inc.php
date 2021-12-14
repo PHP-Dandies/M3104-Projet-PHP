3de868512e3f6152785587329ff6cabd6d7ef254
antoskins <antonypinel@gmail.com>
2021-12-14 07:38:22 +0000

    fhfgh

--- www/utils.inc.php
+++ www/utils.inc.php
@@ -39,8 +39,7 @@ function start_page()
         </li>
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-success" type="submit" href="" target "_blank">Login</button>
       </form>
     </div>
   </div>
