38bbc17ac40cd8924785c8f15f57e256c2bed05c
Ozeliurs <ozeliurs@gmail.com>
2022-01-06 09:26:33 +0100

    Merge main

--- www/Vue/UserManagementView.php
+++ www/Vue/UserManagementView.php
@@ -5,7 +5,29 @@ start_page("test");
 navbar();
 ?>
     <div class="container" style="margin-top: 5px">
-
+        <h2>Utilisateurs</h2>
+        <table class="striped">
+            <thead>
+            <tr>
+                <th>Pseudonyme</th>
+                <th>Role</th>
+                <th>???</th>
+                <th>Reset MDP</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Albert</td>
+                <td>Table Cell 2</td>
+            </tr>
+            <tr>
+                <td>Table Cell 1</td>
+                <td>Table Cell 2</td>
+                <td>Table Cell 3</td>
+                <td>Table Cell 4</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 <?php
 end_page();
