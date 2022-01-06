70a0a9994e7a32dbd708227c8de969b32a2d8e83
Manaud <manaud57@gmil.com>
2022-01-06 11:45:44 +0100

    Enlèvement des espaces en trop

--- www/Vue/LoginView.php
+++ www/Vue/LoginView.php
@@ -4,33 +4,32 @@ include '../HelperUtils.php';
 start_page("test");
 navbar();
 ?>
-        <section>
-
-            <div class="box">
-                <div class="square" style="--i:0;"></div>
-                <div class="square" style="--i:1;"></div>
-                <div class="square" style="--i:2;"></div>
-                <div class="square" style="--i:3;"></div>
-                <div class="square" style="--i:4;"></div>
-               <div class="container2">
-                   <div class="form">
-                       <h2>Login</h2>
-                       <form>
-                           <div class="inputBox">
-                               <input type="text" placeholder="Identifiant">
-                           </div>
-                           <div class="inputBox">
-                               <input type="password" placeholder="Mot de Passe">
-                           </div>
-                           <div class="inputBox">
-                               <input type="submit" value="Login">
-                           </div>
-                           <p class="forget">Mot de passe oublié ? <a href="#">Clique Ici</a> </p>
-                       </form>
-                   </div>
-               </div>
+    <section>
+        <div class="box">
+            <div class="square" style="--i:0;"></div>
+            <div class="square" style="--i:1;"></div>
+            <div class="square" style="--i:2;"></div>
+            <div class="square" style="--i:3;"></div>
+            <div class="square" style="--i:4;"></div>
+            <div class="container2">
+                <div class="form">
+                    <h2>Login</h2>
+                    <form>
+                        <div class="inputBox">
+                            <input type="text" placeholder="Identifiant">
+                        </div>
+                        <div class="inputBox">
+                            <input type="password" placeholder="Mot de Passe">
+                        </div>
+                        <div class="inputBox">
+                            <input type="submit" value="Login">
+                        </div>
+                        <p class="forget">Mot de passe oublié ? <a href="#">Clique Ici</a> </p>
+                    </form>
+                </div>
             </div>
-        </section>
+        </div>
+    </section>
 <?php
 end_page();
 ?>
