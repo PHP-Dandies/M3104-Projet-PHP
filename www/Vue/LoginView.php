d9e3f077a00c0a5771f8c92cb3df322a21c60ea9
Manaud <manaud57@gmil.com>
2022-01-06 10:55:00 +0100

    Login view

--- www/Vue/LoginView.php
+++ www/Vue/LoginView.php
@@ -1,12 +1,33 @@
 <?php
 $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-include $doc_root.'/HelperUtils.php';
+include '../HelperUtils.php';
 start_page("test");
 navbar();
 ?>
-    <div class="container" style="margin-top: 5px">
-    <div class="test"></div>
-    </div>
+        <section>
+            <div class="color"></div>
+            <div class="color"></div>
+            <div class="color"></div>
+            <div class="box">
+               <div class="container2">
+                   <div class="form">
+                       <h2>Login</h2>
+                       <form>
+                           <div class="inputBox">
+                               <input type="text" placeholder="Identifiant">
+                           </div>
+                           <div class="inputBox">
+                               <input type="password" placeholder="Mot de Passe">
+                           </div>
+                           <div class="inputBox">
+                               <input type="submit" value="Connexion">
+                           </div>
+                           <p class="forget">Mot de passe oublié ?<a href="#">Clique Ici</a> </p>
+                       </form>
+                   </div>
+               </div>
+            </div>
+        </section>
 <?php
 end_page();
 ?>
