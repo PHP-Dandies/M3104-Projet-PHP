3b5126a67a29cc960ad59942352b6f4627f780a8
kylianmargaillan1 <kylian.margaillan@etu.univ-amu.fr>
2022-01-06 14:26:25 +0100

    lien tableau avec des variables

--- www/Views/CampaignManagement/CampaignManagementView.php
+++ www/Views/CampaignManagement/CampaignManagementView.php
@@ -7,22 +7,20 @@ $campaigns = array(array('CAMPAIGN_ID'  => 1,'TITLE' => 'JSP', 'BEG_DATE' => '20
     array('CAMPAIGN_ID'  => 2, 'TITLE' => 'TEST','BEG_DATE' => '2019-01-01', 'END_DATE' => '2019-04-04'),
     array('CAMPAIGN_ID'  => 3, 'TITLE' => 'CAMP', 'BEG_DATE' => '2020-01-01', 'END_DATE' => '2020-04-04'));
 ?>
-?>
+    ?>
     <div class="container" style="margin-top: 5px">
-        <div>
-            <h1 class="text-center"> Campagnes en cours </h1>
-            <?php
-            foreach ($campaigns as $campaign){
-               echo'    <table class="striped">'.PHP_EOL;
-               echo'        <tr>'.PHP_EOL;
-               echo'            <td>'.$campaign["TITLE"].'</td>'.PHP_EOL;
-               echo'            <td>'.$campaign["BEG_DATE"].'</td>'.PHP_EOL;
-               echo'            <td>'.$campaign["END_DATE"].'</td>'.PHP_EOL;
-               echo'            <td><button class="button error">Interrompre</button></td>'.PHP_EOL;
-               echo'         </tr>'.PHP_EOL;
-               echo'    </table>'.PHP_EOL;
-
-            }
+        <h1 class="text-center"> Campagnes en cours </h1>
+        <?php
+        echo'    <table class="striped">'.PHP_EOL;
+        foreach ($campaigns as $campaign){
+            echo'        <tr>'.PHP_EOL;
+            echo'            <td>'.$campaign["TITLE"].'</td>'.PHP_EOL;
+            echo'            <td>'.$campaign["BEG_DATE"].'</td>'.PHP_EOL;
+            echo'            <td>'.$campaign["END_DATE"].'</td>'.PHP_EOL;
+            echo'            <td><button class="button error">Interrompre</button></td>'.PHP_EOL;
+            echo'        </tr>'.PHP_EOL;
+        }
+        echo'    </table>'.PHP_EOL;
         ?>
         <br>
         <br>
@@ -39,7 +37,6 @@ $campaigns = array(array('CAMPAIGN_ID'  => 1,'TITLE' => 'JSP', 'BEG_DATE' => '20
                     </a></p>
             </div>
         </details>
-    </div>
     </div>
 <?php
 end_page();
