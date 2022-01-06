fb068571ce0ccc560f7d54a10c535985fd6e342c
Ozeliurs <ozeliurs@gmail.com>
2022-01-06 16:17:55 +0100

    Branch A partir d'ICI

--- www/Views/Idea/IdeaEditionView.php
+++ www/Views/Idea/IdeaEditionView.php
@@ -5,7 +5,25 @@ start_page("test");
 navbar();
 ?>
     <div class="container" style="margin-top: 5px">
-
+        <div class="is-vertical-align is-horizontal-align" style="margin-top: 5px; height: 20vh; background-image: url('../../Images/0.jpg'); background-position: center; background-size: cover;">
+            <h1 class="text-uppercase" style="background-color: rgba(160, 160, 160, 0.64); padding: 5px; color: white"><input type="text" placeholder="Insérer le titre"></h1>
+        </div>
+        <div>
+            <div class="row" style="margin-top: 5px">
+                <div class="col-8">
+                    <textarea rows="15" placeholder="Ecrivez la description de voter projet ici" maxlength="500"></textarea>
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <h3><input type="text" placeholder="Nom organisateur"></h3>
+                        <progress value="60" max="100"></progress>
+                        <p class="is-horizontal-align">XXX sur <input type="text" placeholder="Max point" style="width:20px;height:20px;">  pts</p>
+                    </div>
+                    <br>
+                    <a class="button primary">+ Contenu Débloquable </a>
+                </div>
+            </div>
+        </div>
     </div>
 <?php
 end_page();
--- www/Views/Jury/JuryView.php
+++ www/Views/Jury/JuryView.php
@@ -1,18 +1,58 @@
 <?php
 $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-include $doc_root.'/HelperUtils.php';
-start_page("test");
+include $doc_root.'/Utils/HelperUtils.php';
+include $doc_root.'/Utils/database.php';
+start_page("Jury");
 navbar();
+
+$query = 'SELECT * FROM IDEA';
+$Result = database::execute_query($query);
 ?>
     <div class="container" style="margin-top: 5px">
-        <h2>Campagne XXX</h2>
-        <table class="striped" style="margin-top: 10px">
+        <table>
+            <caption>
+                CAMPAGNE
+            </caption>
+            <thead>
             <tr>
-                <td>Albert</td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <th>IDEE</th>
+                <th>VOIR</th>
+                <th>METTRE EN OEUVRE</th>
+                <th>ABANDONNER</th>
             </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($Result as $res){?>
+            <tr>
+                <td><?php echo $res['TITLE']?> </td>
+                <td><a class="button outline primary">Voir</a></td>
+                <td><a class="button outline primary">Mettre en oeuvre</a></td>
+                <td><a class="button outline">Abandonner</a></td>
+            </tr>
+            <?php }?>
+            </tbody>
+        </table>
+        <br>
+        <a class="button primary">Terminer la campagne</a>
+        <br>
+        <table>
+            <caption>
+                ANCIENNES CAMPAGNES
+            </caption>
+            <thead>
+            <tr>
+                <th>IDEE</th>
+                <th>VOIR</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($Result as $res){?>
+            <tr>
+                <td>Idée 1</td>
+                <td><a class="button outline primary">Voir</a></td>
+            </tr>
+            <?php }?>
+            </tbody>
         </table>
     </div>
 <?php
