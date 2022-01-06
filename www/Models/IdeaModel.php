8ebe9690fe0c02945a24132cd05b0195ac7d8dfa
martin <martin.moreira-de-jesus@etu.univ-amu.fr>
2022-01-06 14:04:54 +0100

    cleanup

---
+++ www/Models/IdeaModel.php
@@ -0,0 +1,9 @@
+<?php
+
+class IdeaModel
+{
+    function __construct()
+    {
+
+    }
+}
--- www/test.php
+++ www/test.php
@@ -1,4 +1,25 @@
 <?php
-echo "Before end !";
-die();
-echo "After end !";
+
+include("Utils/database.php");
+
+// $query = 'INSERT INTO \'test\' (\'ID\', \'intc\', \'charc\') VALUES (NULL, \'3\', \'b\')';
+
+$query = 'SELECT * FROM `test`' ;
+try {
+    echo 'trying to select * <br/>';
+
+    $result_array = database::execute_query($query);
+
+    foreach ($result_array as $array) {
+        foreach ($array as $key => $value) {
+            echo $key . '=>' . $value . '<br/>';
+        }
+        echo '<br/>';
+    }
+
+    echo 'done !<br/>';
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
