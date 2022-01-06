458e3b1202d7c1af012e3edc303e74d6c856ae48
kylianmargaillan1 <kylian.margaillan@etu.univ-amu.fr>
2022-01-06 11:56:32 +0100

    Vue campaign management

--- www/Vue/CampaignManagementView.php
+++ www/Vue/CampaignManagementView.php
@@ -5,7 +5,44 @@ start_page("test");
 navbar();
 ?>
     <div class="container" style="margin-top: 5px">
-
+        <div>
+            <h1 class="text-center"> Campagnes en cours </h1>
+            <table class="striped">
+                <tr>
+                    <td>Nom Campagne 1</td>
+                    <td>DateDeb</td>
+                    <td>DateFin</td>
+                    <td><button class="button error">Interrompre</button></td>
+                </tr>
+                <tr>
+                    <td>Nom Campagn 2</td>
+                    <td>DateDeb</td>
+                    <td>DateFin</td>
+                    <td><button class="button error">Interrompre</button></td>
+                </tr>
+                <tr>
+                    <td>Nom Campagne 3</td>
+                    <td>DateDeb</td>
+                    <td>DateFin</td>
+                    <td><button class="button error">Interrompre</button></td>
+                </tr>
+            </table>
+        </div>
+        <br>
+        <br>
+        <details class="text-center" class=dropdown>
+            <summary class="button success">Créer une nouvelle Campagne ↓</summary>
+            <div class=card>
+                <p><a>
+                    <input type="text" placeholder="Entrez le nom de votre nouvelle campagne">
+                </a></p>
+                <p><a class="text-success">Date du début de cette nouvelle campagne : DateDeb</a></p>
+                <p><a class="text-error">Date fin de cette nouvelle campagne : DateFin</a></p>
+                <p><a>
+                    <button class="button success">Enregistrer</button>
+                </a></p>
+            </div>
+        </details>
     </div>
 <?php
 end_page();
