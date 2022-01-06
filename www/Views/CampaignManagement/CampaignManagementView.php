4a6e0f5497f02790b2b7aec431427cf75609f8e2
kylianmargaillan1 <kylian.margaillan@etu.univ-amu.fr>
2022-01-06 14:16:26 +0100

    Php begin

--- www/Views/CampaignManagement/CampaignManagementView.php
+++ www/Views/CampaignManagement/CampaignManagementView.php
@@ -3,31 +3,27 @@ $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FI
 include $doc_root.'/HelperUtils.php';
 start_page("test");
 navbar();
+$campaigns = array(array('CAMPAIGN_ID'  => 1,'TITLE' => 'JSP', 'BEG_DATE' => '2021-01-01', 'END_DATE' => '2021-04-04'),
+    array('CAMPAIGN_ID'  => 2, 'TITLE' => 'TEST','BEG_DATE' => '2019-01-01', 'END_DATE' => '2019-04-04'),
+    array('CAMPAIGN_ID'  => 3, 'TITLE' => 'CAMP', 'BEG_DATE' => '2020-01-01', 'END_DATE' => '2020-04-04'));
+?>
 ?>
     <div class="container" style="margin-top: 5px">
         <div>
             <h1 class="text-center"> Campagnes en cours </h1>
-            <table class="striped">
-                <tr>
-                    <td>Nom Campagne 1</td>
-                    <td>DateDeb</td>
-                    <td>DateFin</td>
-                    <td><button class="button error">Interrompre</button></td>
-                </tr>
-                <tr>
-                    <td>Nom Campagne 2</td>
-                    <td>DateDeb</td>
-                    <td>DateFin</td>
-                    <td><button class="button error">Interrompre</button></td>
-                </tr>
-                <tr>
-                    <td>Nom Campagne 3</td>
-                    <td>DateDeb</td>
-                    <td>DateFin</td>
-                    <td><button class="button error">Interrompre</button></td>
-                </tr>
-            </table>
-        </div>
+            <?php
+            foreach ($campaigns as $campaign){
+               echo'    <table class="striped">'.PHP_EOL;
+               echo'        <tr>'.PHP_EOL;
+               echo'            <td>'.$campaign["TITLE"].'</td>'.PHP_EOL;
+               echo'            <td>'.$campaign["BEG_DATE"].'</td>'.PHP_EOL;
+               echo'            <td>'.$campaign["END_DATE"].'</td>'.PHP_EOL;
+               echo'            <td><button class="button error">Interrompre</button></td>'.PHP_EOL;
+               echo'         </tr>'.PHP_EOL;
+               echo'    </table>'.PHP_EOL;
+
+            }
+        ?>
         <br>
         <br>
         <details class="text-center" class=dropdown>
