31a012e2e9a46ebff437be94b3a099cf0443a977
antoskins <75854261+antonypinel@users.noreply.github.com>
2022-01-10 12:22:10 +0100

    Update IdeaController.php

--- www/Controllers/IdeaController.php
+++ www/Controllers/IdeaController.php
@@ -7,6 +7,7 @@ class IdeaController {
      * @throws Exception
      */
     public function read($campaign_id) {
+        'SELECT * FROM IDEA WHERE CAMPAIGN_ID = ' . $id;
         $ideas = IdeaModel::get_ideas($campaign_id);
         ViewHelper::display(
             $this,
