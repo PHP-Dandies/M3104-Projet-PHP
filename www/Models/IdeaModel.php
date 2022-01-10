54f9256507b128ec1f1ada7cc3f5c58d70399d2d
kylianmargaillan1 <kylian.margaillan@etu.univ-amu.fr>
2022-01-10 11:15:58 +0100

    getidea by title dans IdeaModel

--- www/Models/IdeaModel.php
+++ www/Models/IdeaModel.php
@@ -13,10 +13,20 @@ class IdeaModel
     /**
      * @throws Exception
      */
-    public static function get_idea(int $id): ?Idea {
+    public static function get_idea_by_id(int $id): ?Idea {
         $ideas = self::get_ideas();
         foreach ($ideas as $idea){
             if ($idea->getIdeaId = $id){
+                return $idea;
+            }
+        }
+        return null;
+    }
+
+    public static function get_idea_by_title(int $title): ?Idea {
+        $ideas = self::get_ideas();
+        foreach ($ideas as $idea){
+            if ($idea->getTitle = $title){
                 return $idea;
             }
         }
