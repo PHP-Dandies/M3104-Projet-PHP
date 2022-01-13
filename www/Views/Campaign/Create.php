2ddd1cdf9d430183748e5eed2b4ab30bb55eae71
kylianmargaillan1 <kylian.margaillan@etu.univ-amu.fr>
2022-01-13 17:22:36 +0100

    AddCampaign ajoute desormais les points aux donateurs

--- www/Views/Campaign/Create.php
+++ www/Views/Campaign/Create.php
@@ -10,10 +10,11 @@ navbar();
 
             <summary  class="button success">Créer une nouvelle Campagne  ↓ </summary>
             <div class=card>
-                <form action="../../Scripts/AddCampaign.php" method="post">
-                <p><input type="text" name="title" id "title" maxlength="25" placeholder="Entrez le nom de votre nouvelle campagne"></p>
+                <form action="../../Scripts/AddCampaign.php", method="post">
+                <p class="text-error">Entrer le titre de la campagne : <input type="text" name="title" id "title" maxlength="25" placeholder="Entrez le nom de votre nouvelle campagne"></p>
                 <p class="text-success"> Sélectionner le début de la campagne : <input type="date" name="datedeb" id="datedeb" class="text-success" ></p>
                 <p class="text-error">Sélectionner la fin de la campagne : <input type="date" name="datefin" id="datefin" class="text-error"></p>
+                <p class="text-success">Sélectionner le nombre de points à attribuer aux donateurs : <input type="number" name="points" id="points" min="1"></p>
                 <p><a>
                         <input type="submit" name="test" value="Enregistrer" >
                     </a></p>
