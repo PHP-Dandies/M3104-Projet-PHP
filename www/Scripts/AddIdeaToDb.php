9516d42fe917d99f79c0e09dc1edd2a47d0ee95b
BILLY Maxime <ozeliurs@gmail.com>
2022-01-14 15:51:10 +0100

    Update AddIdeaToDb.php

--- www/Scripts/AddIdeaToDb.php
+++ www/Scripts/AddIdeaToDb.php
@@ -8,11 +8,11 @@ $goal = $_POST['goal'];
 $picture = $_POST['image'];
 
 // Where the file is going to be placed
-$file = time().'.'.substr(strrchr($_FILES['images']['name'], '.'), 1);
+$file = time().'.'.substr(strrchr($_FILES['image']['name'], '.'), 1);
 $rel_path = '/images/'.$file;
 $target_path = $doc_root.$rel_path;
 
-if(!move_uploaded_file($_FILES['images']['tmp_name'], $target_path)) {
+if(!move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
     header("HTTP/1.1 500 Internal Server Error");
     die();
 }
