a5fd61be917830019f65360141ba9f3d8f9d30f9
Ozeliurs <ozeliurs@gmail.com>
2022-01-14 18:45:04 +0100

    debug

--- www/Views/ViewHelper.php
+++ www/Views/ViewHelper.php
@@ -4,10 +4,6 @@ class ViewHelper
     public static function display($controller_instance, $view_name, $data = array())
     {
         extract($data, EXTR_OVERWRITE);
-        echo dirname(__DIR__) . '/Views/'
-        . str_replace('Controller', '', get_class($controller_instance))
-        . '/' . $view_name
-        . '.php';
         include dirname(__DIR__) . '/Views/'
             . str_replace('Controller', '', get_class($controller_instance))
             . '/' . $view_name
