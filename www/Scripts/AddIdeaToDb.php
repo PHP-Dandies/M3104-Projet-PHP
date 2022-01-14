6f23955566950089b8e00f26d5fab77c5f4c2372
BILLY Maxime <ozeliurs@gmail.com>
2022-01-14 15:42:40 +0100

    Update AddIdeaToDb.php

--- www/Scripts/AddIdeaToDb.php
+++ www/Scripts/AddIdeaToDb.php
@@ -2,19 +2,17 @@
 $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
 include('../Utils/Database.php');
 
-$title = $_POST['TITLE'];
-$description = $_POST['DESCRIPTION'];
-$goal = $_POST['GOAL'];
-$picture = $_POST['IMAGES'];
-$user_id = $_POST['USER_ID'];
-$campaign_id = $_POST['CAMPAIGN_ID'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+$goal = $_POST['goal'];
+$picture = $_POST['images'];
 
 // Where the file is going to be placed
-$file = time().'.'.substr(strrchr($_FILES['IMAGES']['name'], '.'), 1);
+$file = time().'.'.substr(strrchr($_FILES['images']['name'], '.'), 1);
 $rel_path = '/images/'.$file;
 $target_path = $doc_root.$rel_path;
 
-if(!move_uploaded_file($_FILES['IMAGES']['tmp_name'], $target_path)) {
+if(!move_uploaded_file($_FILES['images']['tmp_name'], $target_path)) {
     header("HTTP/1.1 500 Internal Server Error");
     die();
 }
