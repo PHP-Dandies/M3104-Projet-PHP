231d437edbd79f665018f7eb404d8ffddf64dc3b
Manaud <manaud57@gmil.com>
2022-01-14 13:09:50 +0100

    Terminé Read.php, style.css et style2.css

--- www/Views/User/Read.php
+++ www/Views/User/Read.php
@@ -5,31 +5,36 @@ start_page("test");
 navbar();
 /** @var TYPE_NAME $data */
 $users = $data;
+
 ?>
 <section>
     <div class="box">
         <!-- Nom actuel -->
-        <h1 class="text-center"> Listes des utilisateurs </h1>
+        <h1 class="proof"> Listes des utilisateurs </h1>
             <?php foreach ($users as $user) { ?>    <form method="post" action="../../Scripts/EditUser.php?id=<?php echo $user['USER_ID'];?>">
-                <div>
+                <div class="te">
                     <table>
-                        <caption>Liste des utilisateurs</caption> <!-- Titre -->
 
                         <thead> <!-- En-tête du tableau -->
-                            <tr class="text">
-                                <th><label for="username_input">Nom d'utilisateur actuel : <?php echo $user["USERNAME"] ?></label></th>
-                                <th><label for="password_input">Mot de passe actuel : <?php echo $user["PASSWORD"] ?></label></th>
-                                <th><label for="role-input">Role actuel : <?php echo $user["ROLE"] ?></label></th>
-                                <?php if ($user['EMAIL'] === ''){ ?>
-                                <th><label for="email_select">Utilisateur non attribué</label></th>
+                            <tr class="row100-head">
+                                <th class="cell100 column1"><label for="username_input">Nom d'utilisateur actuel : <?php echo $user["USERNAME"] ?></label></th>
+                                <th class="cell100 column2"><label for="password_input">Mot de passe actuel : <?php echo $user["PASSWORD"] ?></label></th>
+                                <th class="cell100 column3"><label for="role-input">Role actuel : <?php echo $user["ROLE"] ?></label></th>
+                                <?php if ($user['EMAIL'] === ''){
+                                    $email = 'non attribué';
+                                }
+                                else {
+                                    $email = $user['EMAIL'];
+                                }?>
+                                <th class="cell100 column4"><label for="email_select" >Email : <?php echo $email ?></label></th>
                             </tr>
                         </thead>
 
                         <tbody> <!-- Corps du tableau -->
-                            <tr>
-                                <th><input type="text" id="username_input" name="username" placeholder="<?php echo $user["USERNAME"] ?>"></th>
-                                <th><input type="text" id="password_input" name="password" placeholder="<?php echo $user["PASSWORD"] ?>"></th>
-                                <th>
+                            <tr class="row100 body">
+                                <td class="cell100-row1"><input type="text" id="username_input" name="username" placeholder="<?php echo $user["USERNAME"] ?>"></td>
+                                <td class="cell100-row1"><input type="text" id="password_input" name="password" placeholder="<?php echo $user["PASSWORD"] ?>"></td>
+                                <td>
                                     <label class="custom-selector">
                                         <select name="role">
                                             <option value="">Choisissez une option</option>
@@ -39,28 +44,27 @@ $users = $data;
                                             <option value="voter">Voteur</option>
                                         </select>
                                     </label>
-                                </th>
-                                <th>
-                                    <select name="email" id="email_select">
-                                        <option value="">Choisissez l'email</option>
-                                        <?php
-                                            foreach ($users as $nUser){
-                                                if ($nUser['EMAIL'] !== ''){ ?>
-                                                <option value="<?php echo $nUser['EMAIL']; ?>"><?php echo $nUser['EMAIL']; ?></option>
-                                                <?php
+                                </td>
+                                <th class="rea">
+                                    <label class="custom-selector">
+                                        <select name="email" id="email_select">
+                                            <option value="">Choisissez l'email</option>
+                                            <?php
+                                            if ($user['EMAIL'] === '') {
+                                                foreach ($users as $nUser){
+                                                    if ($nUser['EMAIL'] !== ''){ ?>
+                                                    <option value="<?php echo $nUser['EMAIL']; ?>"><?php echo $nUser['EMAIL']; ?></option>
+                                                    <?php
+                                                }
                                             }
-                                        }
-                                        ?>
-                                    </select>
+                                            ?>
+                                        </select>
+                                    </label>
                                 </th>
                             </tr>
                         </tbody>
 
                         <tfoot> <!-- Pied du tableau -->
-                            <tr>
-                                <?php } else { ?>
-                                <th><label>Email actuel : <?php echo $user['EMAIL'] ?></label></th>
-                            </tr>
                             <?php } ?>
                         </tfoot>
 
@@ -68,7 +72,7 @@ $users = $data;
 
 
                 </div>
-                <label><input value="Modifier" type="submit" class="Login"></label>
+                <label class="te"><input value="Modifier" type="submit" class="Login"></label>
         </form>
         <?php } ?>
         <a href="../../Scripts/AddUser.php" class="button error">Ajouter Utlisateur</a>
