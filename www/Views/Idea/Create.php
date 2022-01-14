7d0f5611c74b8c8b72b19d3fbbde9881cb9e7f7c
BILLY Maxime <ozeliurs@gmail.com>
2022-01-14 15:30:20 +0100

    Update Create.php

--- www/Views/Idea/Create.php
+++ www/Views/Idea/Create.php
@@ -1,6 +1,6 @@
 <?php
 $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-include $doc_root.'/../Utils/HelperUtils.php';
+include once $doc_root.'/../Utils/HelperUtils.php';
 start_page("test");
 navbar();
 ?>
