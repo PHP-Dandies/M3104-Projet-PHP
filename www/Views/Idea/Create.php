a5006fae112bcea05468aade11ff84a80240101f
BILLY Maxime <ozeliurs@gmail.com>
2022-01-14 15:31:47 +0100

    Update Create.php

--- www/Views/Idea/Create.php
+++ www/Views/Idea/Create.php
@@ -1,6 +1,6 @@
 <?php
 $doc_root = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-include once $doc_root.'/../Utils/HelperUtils.php';
+include_once $doc_root.'/../Utils/HelperUtils.php';
 start_page("test");
 navbar();
 ?>
